6f8301c60f0864736ea22328e247a9dfa0ad4f13
Philipp Legner <philipp@mathigon.org>
2018-10-03 02:05:43 +0100

    Fix repeat function

--- src/strings.js
+++ src/strings.js
@@ -47,8 +47,9 @@ export function toCamelCase(str) {
  * @returns {string}
  */
 export function repeat(str, n = 1) {
-  for (let i=1; i<n; ++i) str += str;
-  return str;
+  let result = str;
+  for (let i=1; i<n; ++i) result += str;
+  return result;
 }
 
 
