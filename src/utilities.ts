df8f2f53ddab2f162a9c752f412b1ace55eed829
Philipp Legner <philipp@mathigon.org>
2021-11-02 09:57:25 +0000

    Update utilities.ts

--- src/utilities.ts
+++ src/utilities.ts
@@ -26,11 +26,10 @@ export function isOneOf<T>(x: T, ...values: T[]) {
   return values.includes(x);
 }
 
-type SomeObject = Record<string, unknown>;
 /** Applies default keys to an object. Properties with value `undefined` or `null` will **not** be overwritten. */
-export function applyDefaults<O extends SomeObject, D extends SomeObject>(obj = {} as O, defaults: D) {
+export function applyDefaults<O extends Obj<unknown>, D extends Obj<unknown>>(obj: O, defaults: D) {
   for (const key of Object.keys(defaults)) {
-    if (!Object.prototype.hasOwnProperty.call(obj, key)) (obj as SomeObject)[key] = defaults[key];
+    if (!Object.prototype.hasOwnProperty.call(obj, key)) (obj as Obj<unknown>)[key] = defaults[key];
   }
   return obj as O & D;
 }
